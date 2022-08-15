e4ac47b2480482b17a17439f40b2957cf2465bf5
kawser00 <kawsarmahmud358@gmail.com>
2022-08-15 11:45:51 +0600

    1.3 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
+import Stats from "./components/Stats";
+
+const initialState = [
+    { id: 1, count: 0 },
+    { id: 2, count: 0 },
+]
 
 export default function App() {
+    const [counter, setCounter] = useState(initialState);
+
+    const increment = (id) => {
+        const updatedCounter = counter.map(c => {
+            if (c.id === id) {
+                return {
+                    ...c,
+                    count: c.count + 1
+                }
+            }
+            return { ...c }
+        });
+        setCounter(updatedCounter);
+    };
+
+    const decrement = (id) => {
+        const updatedCounter = counter.map(c => {
+            if (c.id === id) {
+                return {
+                    ...c,
+                    count: c.count - 1
+                }
+            }
+            return { ...c }
+        });
+        setCounter(updatedCounter);
+    };
+
+    const totalCount = () => {
+       return counter.reduce((total, counter) => total + counter.count, 0)
+    }
+
     return (
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -8,9 +47,15 @@ export default function App() {
             </h1>
 
             <div className="max-w-md mx-auto mt-10 space-y-5">
-                <Counter />
-                <Counter />
-                <Counter />
+                {counter.map(count => (
+                    <Counter
+                        key={count.id}
+                        id={count.id}
+                        count={count.count}
+                        increment={increment}
+                        decrement={decrement}
+                    />))}
+                <Stats count={totalCount()} />
             </div>
         </div>
     );
